313fcf2424eae2bd8a589c1e2e236d88236433dc
AlinaMalinafff <150148650+AlinaMalinafff@users.noreply.github.com>
2024-03-26 11:14:39 +0300

    feat(code+scene): Coroutine+Sliders implemention
    1) Coroutine with blocks buttons and make a pause before gaining a resource
    2) Sliders for depicting coroutine process

--- HW_1/Assets/_Source/Core/ProductionBuilding.cs
+++ HW_1/Assets/_Source/Core/ProductionBuilding.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -11,11 +12,27 @@ namespace _Source.Core
     [FormerlySerializedAs("Slider")] public Slider _slider;
     [FormerlySerializedAs("ProductionTime")] public int _productionTime;
     [FormerlySerializedAs("VisualComponent")] public ResourceVisual _visualComponent;
+    [FormerlySerializedAs("Button")] public Button _button;
     
     public void ButtonClick()
     {
+      _button.interactable = false;
+      StartCoroutine(ClickActions());
+    }
+
+    private IEnumerator ClickActions()
+    {
+      float timeCounter = 0;
+      while (timeCounter < _productionTime)
+      {
+        timeCounter += Time.deltaTime;
+        _slider.value = timeCounter / _productionTime;
+        yield return null;
+      }
       _resourceBank.ChangeResource(_gameResource, 1);
       _visualComponent.UpdateText(_resourceBank.GetResource(_gameResource).Value);
+      _button.interactable = true;
+      _slider.value = 0;
     }
   }
 }
